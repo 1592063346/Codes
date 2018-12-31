0e8a6669d67c17193fb27eba5b8ecf12a39f5455
1592063346 <39879513+1592063346@users.noreply.github.com>
2018-12-31 14:13:33 +0800

    Update 1.cpp

--- Codes(since 12-01)/luogu (2018.12.30)/1.cpp
+++ Codes(since 12-01)/luogu (2018.12.30)/1.cpp
@@ -9,10 +9,10 @@ int main() {
   scanf("%d", &tt);
   while (tt--) {
     scanf("%d%d", &n, &m);
-    int p = (long long)(n - 1) * (m - 1) % (mod - 1), v = 2, result = 1;
-    for (; p; p >>= 1, v = (long long)v * v % mod) {
+    int p = (long long) (n - 1) * (m - 1) % (mod - 1), v = 2, result = 1;
+    for (; p; p >>= 1, v = (long long) v * v % mod) {
       if (p & 1) {
-        result = (long long)result * v % mod;
+        result = (long long) result * v % mod;
       }
     }
     printf("%d\n", result);
