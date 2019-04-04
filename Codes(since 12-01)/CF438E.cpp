7fa815c855c0375015737b535b5eb68f65ffd590
1592063346 <39879513+1592063346@users.noreply.github.com>
2019-04-04 08:51:52 +0800

    Add files via upload

---
+++ Codes(since 12-01)/CF438E.cpp
@@ -0,0 +1,173 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int N = 3e5 + 10, mod = 998244353, root = 3;
+
+void add(int& x, int y) {
+  x += y;
+  if (x >= mod) {
+    x -= mod;
+  }
+}
+
+void sub(int& x, int y) {
+  x -= y;
+  if (x < 0) {
+    x += mod;
+  }
+}
+
+int mul(int x, int y) {
+  return (long long) x * y % mod;
+}
+
+int qpow(int v, int p) {
+  int result = 1;
+  for (; p; p >>= 1, v = mul(v, v)) {
+    if (p & 1) {
+      result = mul(result, v);
+    }
+  }
+  return result;
+}
+
+int n, m, rev[N];
+
+void dft(vector<int>& buffer, bool inv = false) {
+  int n = buffer.size();
+  for (int i = 0; i < n; ++i) {
+    if (i < rev[i]) {
+      swap(buffer[i], buffer[rev[i]]);
+    }
+  }
+  for (int i = 1; i < n; i <<= 1) {
+    int x = qpow(root, inv ? mod - 1 - (mod - 1) / (i << 1) : (mod - 1) / (i << 1));
+    for (int j = 0; j < n; j += i << 1) {
+      int y = 1;
+      for (int k = 0; k < i; ++k, y = mul(y, x)) {
+        int p = buffer[j + k], q = mul(y, buffer[i + j + k]);
+        buffer[j + k] = (p + q) % mod;
+        buffer[i + j + k] = (p - q + mod) % mod;
+      }
+    }
+  }
+  if (inv) {
+    int x = qpow(n, mod - 2);
+    for (int i = 0; i < n; ++i) {
+      buffer[i] = mul(buffer[i], x);
+    }
+  }
+}
+
+vector<int> pmul(vector<int> x, vector<int> y) {
+  int n = x.size() + y.size() - 1, len = 0;
+  for (; (1 << len) < n; ++len);
+  for (int i = 0; i < 1 << len; ++i) {
+    rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << len - 1);
+  }
+  x.resize(1 << len);
+  y.resize(1 << len);
+  dft(x);
+  dft(y);
+  for (int i = 0; i < 1 << len; ++i) {
+    x[i] = mul(x[i], y[i]);
+  }
+  dft(x, true);
+  x.resize(n);
+  return x;
+}
+
+vector<int> pinv(vector<int> x) {
+  int n = x.size(), m = n + 1 >> 1;
+  if (n == 1) {
+    return vector<int>(1, qpow(x[0], mod - 2));
+  } else {
+    vector<int> y = pinv(vector<int>(x.begin(), x.begin() + m)), z = y;
+    y = pmul(y, y);
+    y.resize(n);
+    y = pmul(y, x);
+    z.resize(n);
+    vector<int> result(n);
+    for (int i = 0; i < n; ++i) {
+      result[i] = ((2 * z[i] - y[i]) % mod + mod) % mod;
+    }
+    return result;
+  }
+}
+
+vector<int> pder(vector<int> x) {
+  int n = x.size();
+  vector<int> y(n - 1);
+  for (int i = 0; i + 1 < n; ++i) {
+    y[i] = mul(i + 1, x[i + 1]);
+  }
+  return y;
+}
+
+vector<int> pint(vector<int> x) {
+  int n = x.size();
+  vector<int> y(n + 1), inv(n + 1, 1);
+  for (int i = 2; i <= n; ++i) {
+    inv[i] = mul(mod - mod / i, inv[mod % i]);
+  }
+  for (int i = 1; i <= n; ++i) {
+    y[i] = mul(x[i - 1], inv[i]);
+  }
+  return y;
+}
+
+vector<int> pln(vector<int> x) {
+  int n = x.size();
+  vector<int> y = pint(pmul(pder(x), pinv(x)));
+  y.resize(n);
+  return y;
+}
+
+vector<int> pexp(vector<int> x) {
+  int n = x.size(), m = n + 1 >> 1;
+  if (n == 1) {
+    return vector<int>(1, 1);
+  } else {
+    vector<int> y = pexp(vector<int>(x.begin(), x.begin() + m)), z = y;
+    x.resize(n);
+    y.resize(n);
+    y = pln(y);
+    for (int i = 0; i < n; ++i) {
+      sub(x[i], y[i]);
+    }
+    add(x[0], 1);
+    x = pmul(x, z);
+    x.resize(n);
+    return x;
+  }
+}
+
+vector<int> psqrt(vector<int> x) {
+  int n = x.size(), inv2 = mod + 1 >> 1;
+  vector<int> y = pln(x);
+  for (int i = 0; i < n; ++i) {
+    y[i] = mul(y[i], inv2);
+  }
+  return pexp(y);
+}
+
+int main() {
+  scanf("%d%d", &n, &m);
+  vector<int> g(m + 1);
+  g[0] = 1;
+  for (int i = 1; i <= n; ++i) {
+    int x;
+    scanf("%d", &x);
+    if (x <= m) {
+      g[x] = mod - 4;
+    }
+  }
+  g = psqrt(g);
+  add(g[0], 1);
+  g = pinv(g);
+  for (int i = 1; i <= m; ++i) {
+    printf("%d\n", mul(g[i], 2));
+  }
+  return 0;
+}
