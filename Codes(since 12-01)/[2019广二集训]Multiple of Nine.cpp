cfed49b557a407f17eb6bff325ac6d10b0d36b32
1592063346 <39879513+1592063346@users.noreply.github.com>
2019-06-28 12:48:38 +0800

    Add files via upload

--- Codes(since 12-01)/[2019广二集训]Multiple of Nine.cpp
+++ Codes(since 12-01)/[2019广二集训]Multiple of Nine.cpp
@@ -11,11 +11,18 @@ void add(int& x, int y) {
   }
 }
 
+void sub(int& x, int y) {
+  x -= y;
+  if (x < 0) {
+    x += mod;
+  }
+}
+
 int mul(int x, int y) {
   return (long long) x * y % mod;
 }
 
-int qpow(int x, int y) {
+int power(int x, int y) {
   int result = 1;
   for (; y; y >>= 1, x = mul(x, x)) {
     if (y & 1) {
@@ -25,22 +32,27 @@ int qpow(int x, int y) {
   return result;
 }
 
-int n, q, t, l[N], r[N], ways[1 << 15], dp[10][1 << 15];
-pair<int, int> h[N];
-bool tag[N];
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   cout.tie(0);
+  int n, q, t = 0;
   cin >> n >> q;
+  vector<int> inv(q + 1, 1), ifac(q + 1, 1);
+  for (int i = 2; i <= q; ++i) {
+    inv[i] = mul(mod - mod / i, inv[mod % i]);
+    ifac[i] = mul(ifac[i - 1], inv[i]);
+  }
+  vector<int> l(q + 1), r(q + 1);
+  vector<pair<int, int>> h((q << 1) + 1);
+  vector<bool> tag((q << 1) + 1);
   for (int i = 1; i <= q; ++i) {
     cin >> l[i] >> r[i];
     --l[i];
     h[++t] = make_pair(l[i], i);
     h[++t] = make_pair(r[i], -i);
   }
-  sort(h + 1, h + 1 + t);
+  sort(h.begin() + 1, h.begin() + 1 + t);
   for (int i = 1; i <= t; ++i) {
     if (h[i].second > 0) {
       l[h[i].second] = i;
@@ -49,7 +61,7 @@ int main() {
     }
   }
   auto f = [&] (int n, bool diff) {
-    int x = mul(qpow(10, n) - 1, (mod + 1) / 9);
+    int x = mul(power(10, n) - 1, (mod + 1) / 9);
     if (!diff) {
       ++x;
     }
@@ -91,19 +103,61 @@ int main() {
       }
     }
   }
-  dp[0][0] = 1;
-  int answer = 0;
-  for (int i = 1, j = 1; i <= min(q, 9); j = mul(j, 9 - i), ++i) {
-    for (int j = 1; j < 1 << q; ++j) {
-      for (int k = j; k; k = j & (k - 1)) {
-        int compj = j ^ k;
-        if (k > compj) {
-          add(dp[i][j], mul(dp[i - 1][compj], ways[k]));
+  auto fmt = [&] (vector<int>& buffer) {
+    for (int i = 0; i < q; ++i) {
+      for (int j = 0; j < 1 << q; ++j) {
+        if (j >> i & 1) {
+          add(buffer[j], buffer[j ^ (1 << i)]);
         }
       }
     }
-    add(answer, mul(dp[i][(1 << q) - 1], j));
+  };
+  auto ifmt = [&] (vector<int>& buffer) {
+    for (int i = 0; i < q; ++i) {
+      for (int j = 0; j < 1 << q; ++j) {
+        if (j >> i & 1) {
+          sub(buffer[j], buffer[j ^ (1 << i)]);
+        }
+      }
+    }
+  };
+  auto merge = [&] (vector<int> x, vector<int> y) {
+    vector<int> size(1 << q), result(1 << q);
+    vector<vector<int>> foo(q + 1, vector<int>(1 << q)), bar(q + 1, vector<int>(1 << q));
+    for (int i = 1; i < 1 << q; ++i) {
+      size[i] = size[i - (i & -i)] + 1;
+    }
+    for (int i = 0; i < 1 << q; ++i) {
+      foo[size[i]][i] = x[i];
+      bar[size[i]][i] = y[i];
+    }
+    for (int i = 0; i <= q; ++i) {
+      fmt(foo[i]);
+      fmt(bar[i]);
+    }
+    for (int i = 0; i <= q; ++i) {
+      for (int j = 0; i + j <= q; ++j) {
+        vector<int> t(1 << q);
+        for (int k = 0; k < 1 << q; ++k) {
+          t[k] = mul(foo[i][k], bar[j][k]);
+        }
+        ifmt(t);
+        for (int k = 0; k < 1 << q; ++k) {
+          if (size[k] == i + j) {
+            add(result[k], t[k]);
+          }
+        }
+      }
+    }
+    return result;
+  };
+  vector<vector<int>> dp(10, vector<int>(1 << q));
+  dp[0][0] = 1;
+  int answer = 0;
+  for (int i = 1, j = 1; i <= min(q, 9); j = mul(j, 9 - i), ++i) {
+    dp[i] = merge(dp[i - 1], ways);
+    add(answer, mul(mul(dp[i][(1 << q) - 1], j), ifac[i]));
   }
-  cout << mul(answer, qpow(10, n - h[t].first)) << '\n';
+  cout << mul(answer, power(10, n - h[t].first)) << '\n';
   return 0;
 }
